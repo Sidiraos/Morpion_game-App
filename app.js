5f8959ea86b786ff4c80dbca71cb14eee5eefa86
sidiraos <raossidi@gmail.com>
2023-03-17 20:38:13 +0300

    V1.2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const currentGame = ["","","","","","","","",""];
-
-
+const currentGame = [];
+let xCombinaison = [];
+let yCombinaison = [];
 const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,4 +12,60 @@ const winningCombinations = [
     [2, 4, 6]
 ]
 
+const cases = [...document.querySelectorAll('.case')];
+const player = document.querySelector('h1[data-player="x"]');
+let lock = false;
+cases.forEach(caseEl => {
+    caseEl.addEventListener('click', handleClick);
+})
 
+function handleClick(e) {
+    fullCase(e.target , player)
+    if(e.target.textContent === 'X') {
+        xCombinaison.push(cases.findIndex(el => el === e.target)); 
+        console.log("xCombinaison" , xCombinaison);
+        checkWinner(xCombinaison , "X")
+    } else {
+        yCombinaison.push(cases.findIndex(el => el === e.target)); 
+        console.log("yCombinaison" , yCombinaison);
+        checkWinner(yCombinaison , "Y")
+    }
+    if (e.target.textContent.length > 0) {
+        e.target.removeEventListener('click', handleClick);
+    }
+}
+
+function checkWinner(array , joueur) {
+    array.sort()
+    console.log("array", array);
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const combination = winningCombinations[i];
+        // Vérifier si tous les éléments de la combinaison se trouvent dans l'array
+        if (combination.every((element) => array.includes(element))) {
+          console.log("Combinaison gagnante trouvée :", combination);
+          // Arrêter la boucle si une combinaison gagnante est trouvée
+            player.textContent = joueur + " vous avez gagné! Appuiyez sur F5 pour recommencez";
+            player.classList.add("fw-lighter");
+            reloadGame();
+          break;
+        }
+      }
+
+    }
+
+function reloadGame(){
+    document.addEventListener('keydown', (e)=>{
+        if(e.key === 'F5')location.reload();
+    })
+}
+function fullCase(el , player){
+    if(player.getAttribute('data-player') === 'x') {
+        el.textContent = 'X';
+        player.setAttribute('data-player', 'o');
+        player.textContent = 'Au tour de 0';
+    } else {
+        el.textContent = 'O';
+        player.setAttribute('data-player', 'x');
+        player.textContent = 'Au tour de X';
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+<h1>Morpion Game App </h1>
+
+<ul>
+    <b>V1.0</b>
+    <li>design finished</li>
+    <li>No Js Yet</li>
+</ul>
+
+<ul>
+    <b>V1.2</b>
+    <li>add JS features</li>
+    <li>worked game</li>
+</ul>
